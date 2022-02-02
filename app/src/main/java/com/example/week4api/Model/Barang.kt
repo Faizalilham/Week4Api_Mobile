a765688ff7f328e5444cca4978eea455d8a8d74b
Faizalilham <faizalfalakh19@gmail.com>
2022-02-02 17:13:13 +0700

    Week 4 Api

---
+++ app/src/main/java/com/example/week4api/Model/Barang.kt
@@ -0,0 +1,21 @@
+package com.example.week4api.Model
+
+data class Barang (
+    val id : Int,
+    val nama : String,
+    val kode : Int,
+    val createdAt : String,
+    val updatedAt : String
+    )
+
+data class ListResponse<T>(
+    val msg : String,
+    val status : Int,
+    val data : MutableList<T>
+)
+
+data class SingleResponse<T>(
+    val msg : String,
+    val status : Int,
+    val data : T
+)
---
+++ app/src/main/java/com/example/week4api/webService/ApiEndPoint.kt
@@ -0,0 +1,23 @@
+package com.example.week4api.webService
+
+import com.example.week4api.Model.Barang
+import com.example.week4api.Model.ListResponse
+import com.example.week4api.Model.SingleResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiEndPoint {
+
+    @GET("barang")
+    fun getData():Call<ListResponse<Barang>>
+
+    @GET("barang/{id}")
+    fun getDataById(@Path("id")id : Int):Call<SingleResponse<Barang>>
+
+    @FormUrlEncoded
+    @POST("barang")
+    fun PostData(@Field("nama")nama : String,
+                 @Field("kode")kode : Int):Call<SingleResponse<Barang>>
+
+
+}
